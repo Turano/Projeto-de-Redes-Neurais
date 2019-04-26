a6330a6d95c66c4f20159ff3ce39b0d813e69814
Turano <42282426+Turano@users.noreply.github.com>
2019-04-26 01:12:18 -0300

    Add files via upload

--- Trabalho.py
+++ Trabalho.py
@@ -1,105 +1,96 @@
 import math
 import numpy as np
 
-#Função de Custo MSE
-def MSE(output, lista):
-    return ((output - lista)**2)/2 
+# Modelo do perceptron
+def perc(num, peso, wbias):
+    return 1/(1+(math.exp(-(sum(multiplicar(num, peso), wbias)))))
 
-# Esquece
 def multiplicar(num, peso):
     for x in range(len(num)):
         num[x] *= peso[x]
     return num
 
+def feedforward(whl, wout, hl, outnosig, hlnosig, output, input, wbhl, wbout):
 
-# Declaração do modelo do perceptron
-def perc(num, peso, bias):
-    return 1/(1+(math.exp(-(sum(multiplicar(num, peso), bias))))) 
+    transpostahl = np.matrix.transpose(whl)
 
-#Declaro os pesos iniciais, por meio da função de He-et-al
-whl = np.random.randn(256, 16) * np.sqrt(2 / 256)
+    transpostaout = np.matrix.transpose(wout)
 
-wout = np.random.randn(16, 10) * np.sqrt(2 / 16)
+    for x in range (30):
+        hl[x] = perc(input, transpostahl[x], wbhl[x])
+        hlnosig[x] = sum(multiplicar(input, transpostahl[x]), wbhl[x])
 
-awhl = np.random.randn(256, 16) * np.sqrt(2 / 256)
-
-awout = np.random.randn(16, 10) * np.sqrt(2 / 16)
-
-taxa = 0.2
-
-erro = np.zeros(1593)
-
-# Abro o arquivo de imagem
-data = open("semeion.data", "r")
-#weights = open("pesos", "w+")
-error = open("error", "w+")
-
-
-input = np.zeros(256)
-algarismo = np.zeros(10)
-
-#Declaro a hidden layer
-hl = np.zeros(16)
-
-#Declaro o output
-output = np.zeros(10)
-
-bias = 0
-
-for z in range(1593):
-
-    #np.savetxt(weights, wout)
-    #np.savetxt(weights, whl)
-
-    list = data.readline().split(" ")
-
-    #Converto os dados de lista para float
-    for x in range (256):
-        input[x] = float(list[x])
     for x in range (10):
-        algarismo[x] = int(list[256+x])
+        output[x] = perc(hl, transpostaout[x], wbout[x])
+        outnosig[x] = sum(multiplicar(hl, transpostaout[x]), wbout[x])
+
+    return (output, outnosig, hlnosig)
+
+def backpropagation(output, algarismo, wout, whl, input, hl, outnosig, hlnosig, wbhl, wbout):
+
+    taxa = 0.3
 
     cost = 0
     costfunction = 0
 
-    transpostahl = np.matrix.transpose(whl)
-    transpostaout = np.matrix.transpose(wout)
-
-    #Passo os valores pelos perceptrons
-    for x in range (16):
-        hl[x] = perc(input, transpostahl[x], bias)
+    awout = wout
+    awhl = whl
 
     for x in range (10):
-        output[x] = perc(hl, transpostaout[x], bias)
+        costfunction += ((output[x] - algarismo[x])**2)/2
+        cost += output[x] - algarismo[x]
 
-    #Aplico a função de custo
-    for x in range (10):
-        costfunction += MSE(output[x], int(algarismo[x]))
-
-    erro[z]= costfunction
-
-    #error.write(str(costfunction))
-    #error.write(" ")
-
-    for x in range (10):
-        cost += output[x] - int(algarismo[x])
-
-    for x in range(16):
+    for x in range(30):
         for y in range(10): 
-            awout[x][y] = wout[x][y] - taxa * (cost * (1/(1+np.exp(-(hl[x]* wout[x][y])))) * (1 - 1/(1+np.exp(-(hl[x]* wout[x][y])))) * hl[x])
+            awout[x][y] = wout[x][y] - taxa * cost * (1/(1+np.exp(-(hl[x] * wout[x][y] + wbout[y])))) * (1 - 1/(1+np.exp(-(hl[x] * wout[x][y] + wbout[y])))) * hl[x]
 
     for x in range(256):
-        for y in range(16):
-            awhl[x][y] = whl[x][y] - taxa * (cost * (1/(1+np.exp(-(hl[y]* sum(wout[y]))))) * (1 - 1/(1+np.exp(-(hl[y]* sum(wout[y]))))) * sum(wout[y]) * (1 - 1/(1+np.exp(-(input[x]* whl[x][y])))) * input[x])
+        for y in range(30):
+            awhl[x][y] = whl[x][y] - taxa * cost * (1/(1+np.exp(-(hl[y]* sum(wout[y]))))) * (1 - 1/(1+np.exp(-(hl[y]* sum(wout[y]))))) * sum(wout[y]) * (1/(1+np.exp(-(input[x]* whl[x][y] + wbhl[y])))) * (1 - 1/(1+np.exp(-(input[x]* whl[x][y] + wbhl[y])))) * input[x]
 
-    wout = awout
-    whl = awhl
+    for x in range (10):    
+        wbout[x] = wbout[x] - taxa * cost * (1/(1+np.exp(-sum(outnosig)))) * (1 - 1/(1+np.exp(-sum(outnosig))))
+    for x in range (30):
+        wbhl[x] = wbhl[x] - taxa * cost * (1/(1+np.exp(-sum(outnosig)))) * (1 - 1/(1+np.exp(-sum(outnosig)))) * sum(wout[x]) * (1/(1+np.exp(-sum(hlnosig)))) * (1 - 1/(1+np.exp(-sum(hlnosig))))
 
-error.write(str(sum(erro)/1593))
+    return (costfunction, awhl, awout, wbhl, wbout)
 
-#Fecho o arquivo
-data.close()
+def main():
+    error = open("error", "a+")
 
-error.close()
+    input = np.zeros(256)
+    algarismo = np.zeros(10)
 
-#weights.close()
+    whl = np.random.randn(256, 30) * np.sqrt(2 / 256)
+    wout = np.random.randn(30, 10) * np.sqrt(2 / 30)
+    wbhl = np.random.random(30)
+    wbout = np.random.random(10)
+
+    hl = np.zeros(30)
+    output = np.zeros(10)
+
+    outnosig = np.zeros(10)
+    hlnosig = np.zeros(30)
+
+    erro = np.zeros(1593)
+
+    for x in range(10):
+        data = open("semeion.data", "r")
+
+        for z in range(1593):
+            adata = data.readline().split(" ")
+            for x in range (256):
+                input[x] = float(adata[x])
+            for x in range (10):
+                algarismo[x] = int(adata[256+x])
+            (output, outnosig, hlnosig) = feedforward(whl, wout, hl, outnosig, hlnosig, output, input, wbhl, wbout)
+            (erro[z], whl, wout, wbhl, wbout) = backpropagation(output, algarismo, wout, whl, input, hl, outnosig, hlnosig, wbhl, wbout)
+
+        data.close()
+        error.write(str(sum(erro)/1593))
+        error.write("\n")
+        print(str(sum(erro)/1593))
+    error.close()
+
+if __name__ == '__main__':
+    main() 
